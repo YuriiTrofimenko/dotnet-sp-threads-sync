d5285ea17c8512fd26092dc0a6546b1e498bb26f
student <student@DESKTOP-L3ET5DL>
2020-02-18 19:35:42 +0200

    Mutex

--- ThreadSyncDemo/Actor.cs
+++ ThreadSyncDemo/Actor.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadSyncDemo
 {
     class Actor
     {
+        private static Mutex mutex = new Mutex();
         public Data data;
         public Actor(Data data)
         {
             this.data = data;
         }
         public static void IncreaseStatic() {
-
+            //mutex.WaitOne();
             Data.countStatic++;
-
+            //mutex.ReleaseMutex();
         }
         public void Increase()
         {
